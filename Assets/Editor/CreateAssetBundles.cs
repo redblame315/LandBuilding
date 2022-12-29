17aa4efe7ce8aac0edd0a7243a13982a38078f90
redblame315 <redblame315@gmail.com>
2022-12-29 05:05:23 -0500

    1.change the asset bundle build directory according to the platform such as pc, webgl, android

--- Assets/Editor/CreateAssetBundles.cs
+++ Assets/Editor/CreateAssetBundles.cs
@@ -5,7 +5,7 @@ public class CreateAssetBundles
     [MenuItem("Assets/Build AssetBundles(PC)")]
     static void BuildAllAssetBundlesPC()
     {
-        string assetBundleDirectory = "Assets/AssetBundles";
+        string assetBundleDirectory = "Assets/AssetBundles_PC";
         if (!Directory.Exists(assetBundleDirectory))
         {
             Directory.CreateDirectory(assetBundleDirectory);
@@ -29,7 +29,7 @@ public class CreateAssetBundles
     [MenuItem("Assets/Build AssetBundles(Android)")]
     static void BuildAllAssetBundlesAndroid()
     {
-        string assetBundleDirectory = "Assets/AssetBundles";
+        string assetBundleDirectory = "Assets/AssetBundles_Android";
         if (!Directory.Exists(assetBundleDirectory))
         {
             Directory.CreateDirectory(assetBundleDirectory);
